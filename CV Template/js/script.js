7c395acf2c200e7a8c274b01540a47d28de945f4
kagame8732 <kagame8732@gmail.com>
2022-12-21 13:34:59 +0200

    changes about responsiveness

---
+++ CV Template/js/script.js
@@ -0,0 +1,8 @@
+const getNewColor = () => {
+  let symbol = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color = color + symbol[Math.floor(Math.random() * 16)];
+  }
+  document.getElementById("heading-position").style.color = color;
+};
